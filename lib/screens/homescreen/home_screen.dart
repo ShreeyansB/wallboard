4c558aec58134e048a7f5c3abdcaf2b11d8e2ba6
BallisticSwami <sbahadkar@rocketmail.com>
2021-07-30 18:01:23 +0530

    Added blur bottom nav bar

--- lib/screens/homescreen/home_screen.dart
+++ lib/screens/homescreen/home_screen.dart
@@ -28,29 +28,67 @@ class _HomePageState extends State<HomePage> {
             )),
         child: Scaffold(
           backgroundColor: Colors.transparent,
-          body: CustomScrollView(
-            physics: BouncingScrollPhysics(),
-            slivers: [
-              SliverAppBar(
-                toolbarHeight:
-                    kToolbarHeight + SizeConfig.safeBlockVertical * 1.4,
-                textTheme: Theme.of(context).textTheme,
-                backgroundColor: Colors.transparent,
-                elevation: 0,
-                title: Text(
-                  "Wallpapers",
-                  style: TextStyle(
-                      fontWeight: FontWeight.w600, letterSpacing: 0.8),
-                ),
-              ),
-              SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (context, index) => Container(
-                    height: 150,
-                    color: Colors.deepPurple,
-                    margin: EdgeInsets.all(20),
+          body: Stack(
+            children: [
+              CustomScrollView(
+                physics: BouncingScrollPhysics(),
+                slivers: [
+                  SliverAppBar(
+                    toolbarHeight:
+                        kToolbarHeight + SizeConfig.safeBlockVertical * 1.4,
+                    textTheme: Theme.of(context).textTheme,
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    title: Text(
+                      "Wallpapers",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600, letterSpacing: 0.8),
+                    ),
                   ),
-                  childCount: 10,
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) => Container(
+                        height: 150,
+                        color: Colors.deepPurple,
+                        margin: EdgeInsets.all(20),
+                      ),
+                      childCount: 10,
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                bottom: 0,
+                child: ClipRect(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 15, sigmaY: 12),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.symmetric(horizontal: 35, vertical: 10),
+                      color: Colors.black54,
+                      child: SalomonBottomBar(
+                        currentIndex: _currentIndex,
+                        onTap: (i) => setState(() => _currentIndex = i),
+                        items: [
+                          SalomonBottomBarItem(
+                            icon: Icon(Icons.home),
+                            title: Text("Home"),
+                            selectedColor: Colors.deepPurpleAccent.shade400,
+                          ),
+                          SalomonBottomBarItem(
+                            icon: Icon(Icons.favorite_border),
+                            title: Text("Likes"),
+                            selectedColor: Colors.deepPurpleAccent.shade400,
+                          ),
+                          SalomonBottomBarItem(
+                            icon: Icon(Icons.search),
+                            title: Text("Search"),
+                            selectedColor: Colors.deepPurpleAccent.shade400,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ],
